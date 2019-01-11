a2800a1a1db79f98a657d233c7c20616bdc5ffcc
Jaya Vunna <you@example.com>
2019-01-11 14:49:27 +0000

    sfffgg

---
+++ src/loveProgramming/MathOperations.java
@@ -0,0 +1,26 @@
+package loveProgramming;
+
+public class MathOperations {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 125;
+        b = 24;
+        c = a+b;
+        System.out.println ("sum of "+ a + " and "+ b + " is " + c);
+
+
+        c = a-b;
+        System.out.println (c);
+
+
+        c = a * b;
+        System.out.println(c);
+        c = a/b;
+        System.out.println(c);
+        c = a % b;
+        System.out.println(c);
+
+
+    }
+}
